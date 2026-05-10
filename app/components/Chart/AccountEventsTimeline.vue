4273258331185bc8d741cbb843a1c11e89482d2e
Alec Lloyd Probert <55991794+graphieros@users.noreply.github.com>
2026-05-10 23:02:19 +0200

    feat: use classic line chart for threshold integration (#110)
    * feat: use classic line chart for threshold integration
    
    * fix: follow clank's advice, add smoother line transitions
    
    * feat: add series for total events
    
    * fix: add area on total series
    
    * feat: add option to display total as bars
    
    * refactor: plug thresholds and cleanup
    
    * fix: don't use nice scale to use full height real estate
    
    * fix: better label for total

--- app/components/Chart/AccountEventsTimeline.vue
+++ app/components/Chart/AccountEventsTimeline.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import {
-  VueUiStackline,
-  type VueUiStacklineDatasetItem,
-  type VueUiStacklineConfig,
-} from "vue-data-ui/vue-ui-stackline";
+  VueUiXy,
+  type VueUiXyDatasetItem,
+  type VueUiXyConfig,
+} from "vue-data-ui";
 import { getCompleteDayRange } from "./chart";
 import type { GitHubEvent, GitHubEventType } from "~~/shared/types/identity";
 import { githubEventTypes } from "~~/shared/types/identity";
-import "vue-data-ui/style.css";
 import { useElementSize } from "@vueuse/core";
 import { useChartTooltipPosition } from "~/composables/useChartTooltipPosition";
 import { useColors } from "~/composables/useColors";
+import { identityConfig } from "@unveil/identity";
 
 import("vue-data-ui/style.css");
 
@@ -20,7 +20,7 @@ const props = defineProps<{
 }>();
 
 const rootEl = shallowRef<HTMLElement | null>(null);
-const chartRef = useTemplateRef("chartRef");
+const chartLineRef = useTemplateRef("chartLineRef");
 
 onMounted(async () => {
   rootEl.value = document.documentElement;
@@ -29,9 +29,13 @@ onMounted(async () => {
 const colors = useColors(rootEl);
 const { width } = useElementSize(rootEl);
 
-const mdBreakpoint = 768;
+const metrics = ["Forks", "New branches", "Pull requests", "Total"];
 
-const isAboveMd = computed(() => width.value >= mdBreakpoint);
+const selectedLegendItems = ref(metrics);
+
+function selectLegend(items: Array<{ color: string; name: string }>) {
+  selectedLegendItems.value = items.map((i) => i.name);
+}
 
 const eventConfig = computed(() => {
   const classification = props.classification || "mixed";
@@ -62,20 +66,60 @@ const eventConfig = computed(() => {
     ForkEvent: {
       name: "Forks",
       color: color.fork,
+      threshold: identityConfig.FORKS_EXTREME,
+      visible: selectedLegendItems.value.includes("Forks"),
+      labelOffsetY: 40,
     },
     CreateEvent: {
       name: "New branches",
       color: color.branch,
+      threshold: null,
+      visible: selectedLegendItems.value.includes("New branches"),
+      labelOffsetY: 0,
     },
     PullRequestEvent: {
       name: "Pull requests",
       color: color.pr,
-    },
-    IssueCommentEvent: {
-      name: "Comments",
-      color: color.comment,
+      threshold: identityConfig.PRS_TODAY_EXTREME,
+      visible: selectedLegendItems.value.includes("Pull requests"),
+      labelOffsetY: 6,
     },
   };
+});
+
+type VueUiXyAnnotation = NonNullable<
+  NonNullable<VueUiXyConfig["chart"]>["annotations"]
+>[number];
+
+/**
+ * NOTE: thresholds are assumed to have different values for each metric.
+ * If 2 metrics share the same threshold, then you might consider merging them into a single one.
+ */
+const thresholds = computed<VueUiXyAnnotation[]>(() => {
+  return Object.values(eventConfig.value)
+    .filter((kpi) => selectedLegendItems.value.includes(kpi.name))
+    .filter((kpi) => !!kpi.threshold)
+    .map((kpi) => ({
+      show: true,
+      yAxis: {
+        yTop: kpi.threshold,
+        label: {
+          position: "start", // or 'end', to alternate if needed to prevent overlap between contiguous labels
+          text: `${kpi.name} (${kpi.threshold})`,
+          offsetX: -12, // if position == 'end', needs to be adapted
+          offsetY: kpi.labelOffsetY,
+          fontSize: 16,
+          backgroundColor: "transparent",
+          color: kpi.color,
+          border: { stroke: "transparent" },
+        },
+        line: {
+          stroke: kpi.color,
+          strokeWidth: 2,
+          strokeDasharray: 6,
+        },
+      },
+    }));
 });
 
 function isGitHubEventType(type: string | null): type is GitHubEventType {
@@ -94,9 +138,13 @@ const eventDays = computed(() => {
 
 const hasEnoughDays = computed<boolean>(() => eventDays.value.length > 1);
 
-function createStacklineDataset(
-  events: GitHubEvent[],
-): VueUiStacklineDatasetItem[] {
+const activeGitHubEventTypes = computed(() => {
+  return githubEventTypes.filter(
+    (eventType) => eventType !== "IssueCommentEvent",
+  );
+});
+
+function createLineDataset(events: GitHubEvent[]): VueUiXyDatasetItem[] {
   const days = getCompleteDayRange(eventDays.value);
 
   const counts: Record<GitHubEventType, Record<string, number>> = {
@@ -116,18 +164,48 @@ function createStacklineDataset(
     counts[event.type][day] = (counts[event.type][day] || 0) + 1;
   }
 
-  return githubEventTypes.map((eventType) => {
-    const config = eventConfig.value[eventType];
-    return {
-      name: config.name,
-      color: config.color,
-      series: days.map((day) => counts[eventType][day] || 0),
-    };
-  });
+  const individualEvents: VueUiXyDatasetItem[] =
+    activeGitHubEventTypes.value.map((eventType) => {
+      const config = eventConfig.value[eventType];
+
+      return {
+        type: "line",
+        useArea: true,
+        smooth: true,
+        name: config.name,
+        color: config.color,
+        series: days.map((day) => counts[eventType][day] || 0),
+      };
+    });
+
+  const totalEvents: VueUiXyDatasetItem = {
+    type: "line",
+    useArea: true,
+    smooth: true,
+    name: "Combined activity",
+    color: colors.value.borderLight,
+    series: days.map((_, index) => {
+      return individualEvents.reduce((total, event) => {
+        return total + Number(event.series[index]);
+      }, 0);
+    }),
+  };
+
+  return [...individualEvents, totalEvents];
 }
 
-const dataset = computed<VueUiStacklineDatasetItem[]>(() => {
-  return createStacklineDataset(props.events);
+const datasetLine = computed(() => createLineDataset(props.events));
+
+const maxValBetweenDatasetAndThresholds = computed(() => {
+  const maxDataset = Math.max(
+    ...datasetLine.value
+      .filter((s) => selectedLegendItems.value.includes(s.name))
+      .flatMap((d) => d.series.map((v) => v ?? 0)),
+  );
+  const maxThreshold = Math.max(
+    ...thresholds.value.map((t) => t.yAxis?.yTop ?? 0),
+  );
+  return Math.max(maxDataset, maxThreshold, 1);
 });
 
 const timestamps = computed<number[]>(() => {
@@ -136,104 +214,174 @@ const timestamps = computed<number[]>(() => {
   );
 });
 
-// true: show as percentages
-const isDistributed = shallowRef(false);
+const tooltipPositionLine = useChartTooltipPosition(chartLineRef);
 
-const tooltipPosition = useChartTooltipPosition(chartRef);
-
-const config = computed<VueUiStacklineConfig>(() => {
-  return {
+const configLine = computed<VueUiXyConfig>(() => ({
+  useCssAnimation: false,
+  line: {
+    useGradient: false,
+    dot: {
+      useSerieColor: false,
+      fill: colors.value.bg,
+      strokeWidth: 1,
+    },
+  },
+  bar: {
+    useGradient: true,
+  },
+  chart: {
     userOptions: { show: false },
-    style: {
-      chart: {
-        backgroundColor: "transparent",
-        grid: {
-          x: {
-            axisColor: colors.value.border,
-            timeLabels: {
-              color: colors.value.textMuted,
-              rotation: -30,
-              autoRotate: {
-                enable: false,
-              },
-              values: timestamps.value,
-              showOnlyAtModulo: true,
-              modulo: 12,
-              datetimeFormatter: {
-                enable: true,
-                useUTC: true,
-                locale: "en",
-                options: {
-                  year: "dd MMM",
-                  month: "dd MMM",
-                  day: "dd MMM",
-                  minute: "dd MMM",
-                  second: "dd MMM",
-                },
-              },
+    backgroundColor: "transparent",
+    color: colors.value.textMuted,
+    annotations: thresholds.value,
+    highlighter: {
+      useLine: true,
+      color: colors.value.textMuted,
+    },
+    grid: {
+      position: "middle",
+      stroke: "transparent",
+      labels: {
+        show: false,
+        yAxis: {
+          scaleMax: maxValBetweenDatasetAndThresholds.value,
+          useNiceScale: false,
+        },
+        xAxisLabels: {
+          show: true,
+          color: colors.value.textMuted,
+          values: timestamps.value,
+          showOnlyAtModulo: true,
+          modulo: 12,
+          rotation: -30,
+          autoRotate: {
+            enable: false,
+          },
+          datetimeFormatter: {
+            enable: true,
+            useUTC: true,
+            locale: "en",
+            options: {
+              year: "dd MMM",
+              month: "dd MMM",
+              day: "dd MMM",
+              minute: "dd MMM",
+              second: "dd MMM",
             },
           },
-          y: {
-            showAxis: false,
-            axisLabels: { show: false },
-          },
-        },
-        highlighter: {
-          useLine: true,
-          color: colors.value.text,
-          opacity: 0,
-        },
-        legend: {
-          backgroundColor: "transparent",
-          color: colors.value.textMuted,
-        },
-        lines: {
-          useArea: true,
-          areaOpacity: 1,
-          smooth: true,
-          distributed: isDistributed.value,
-          gradient: { show: false },
-          totalValues: { show: false },
-          dataLabels: { show: false },
-          path: {
-            useSerieColor: false,
-            stroke: "transparent",
-          },
-          dot: {
-            stroke: "#FFFFFF",
-            radius: 0,
-          },
-        },
-        padding: {
-          left: 48,
-          right: 48,
-        },
-        tooltip: {
-          backgroundColor: colors.value.bg,
-          color: colors.value.text,
-          borderColor: colors.value.border,
-          backgroundOpacity: 30,
-          position: tooltipPosition.value,
-          offsetX: 24,
-          offsetY: -64,
-          fontSize: isAboveMd.value ? undefined : 10,
-        },
-        zoom: {
-          show: false,
         },
       },
     },
-  };
-});
+    zoom: { show: false, keepState: true },
+    tooltip: {
+      backgroundColor: colors.value.bg,
+      color: colors.value.text,
+      borderColor: colors.value.border,
+      backgroundOpacity: 30,
+      position: tooltipPositionLine.value,
+      offsetX: 24,
+      offsetY: -(selectedLegendItems.value.length * 18),
+    },
+  },
+}));
 </script>
 
 <template>
   <ClientOnly>
-    <VueUiStackline
+    <VueUiXy
       v-if="hasEnoughDays"
-      ref="chartRef"
-      :dataset="dataset"
-      :config="config"
-    />
+      ref="chartLineRef"
+      :dataset="datasetLine"
+      :config="configLine"
+      @selectLegend="selectLegend"
+    >
+      <template #area-gradient="{ series, id }">
+        <linearGradient :id="id" x1="0" x2="0" y1="0" y2="1">
+          <stop offset="0%" :stop-color="series.color" stop-opacity="0.3" />
+          <stop offset="100%" :stop-color="colors.bg" stop-opacity="0" />
+        </linearGradient>
+      </template>
+
+      <!-- Remove this if you don't want the total series as bars -->
+      <template #bar-gradient="{ series, positiveId }">
+        <linearGradient :id="positiveId" x1="0" x2="0" y1="0" y2="1">
+          <stop offset="0%" :stop-color="series.color" stop-opacity="0.3" />
+          <stop offset="100%" :stop-color="colors.bg" stop-opacity="0" />
+        </linearGradient>
+      </template>
+
+      <!-- Custom tooltip -->
+      <template #tooltip="{ datapoint, timeLabel }">
+        <div class="flex flex-col">
+          <div class="mb-1">{{ timeLabel.text }}</div>
+          <div
+            class="flex flex-row gap-2 items-center"
+            v-for="series in datapoint"
+            :key="`${series.name}-${series.absoluteIndex}`"
+          >
+            <div class="h-2 w-2">
+              <svg viewBox="0 0 2 2" class="w-full h-full">
+                <circle cx="1" cy="1" r="1" :fill="series.color" />
+              </svg>
+            </div>
+            <span :style="{ color: colors.textMuted }">{{ series.name }}</span>
+            <span>{{ series.value }}</span>
+          </div>
+        </div>
+      </template>
+
+      <!-- Custom legend -->
+      <template #legend="{ legend }">
+        <div class="flex flex-row gap-4 justify-center mt-2">
+          <button
+            class="flex flex-row gap-1.5 place-items-center"
+            :class="item.isSegregated ? 'opacity-50' : 'hover:underline'"
+            v-for="item in legend"
+            @click="item.segregate()"
+          >
+            <div class="w-2 h-2">
+              <svg viewBox="0 0 2 2" class="w-full h-full">
+                <circle :cx="1" :cy="1" :r="1" :fill="item.color" />
+              </svg>
+            </div>
+            <div :class="`text-sm ${item.isSegregated ? 'line-through' : ''}`">
+              {{ item.name }}
+            </div>
+          </button>
+        </div>
+      </template>
+    </VueUiXy>
   </ClientOnly>
 </template>
+
+<style scoped>
+:deep(.vue-data-ui-component) {
+  --super-ease-out: cubic-bezier(0.15, 0.75, 0.35, 1);
+}
+
+:deep(.vue-data-ui-component .serie_line_0 path),
+:deep(.vue-data-ui-component .serie_line_1 path),
+:deep(.vue-data-ui-component .serie_line_2 path),
+:deep(.vue-data-ui-component .serie_line_3 path),
+.svg-element-transition,
+:deep(.vdui-shape-circle) {
+  transition: all 0.5s var(--super-ease-out) !important;
+}
+
+@media (prefers-reduced-motion: reduce) {
+  :deep(.vue-data-ui-component .serie_line_0 path),
+  :deep(.vue-data-ui-component .serie_line_1 path),
+  :deep(.vue-data-ui-component .serie_line_2 path),
+  :deep(.vue-data-ui-component .serie_line_3 path),
+  .svg-element-transition,
+  :deep(.vdui-shape-circle) {
+    transition: none !important;
+  }
+}
+
+:deep(.vue-ui-xy-annotation-label) {
+  stroke: var(--bg);
+  stroke-width: 4;
+  paint-order: stroke;
+}
+</style>
