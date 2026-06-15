7bd259738620a053932f074afdd85c9b5f80c41e
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-06-15 21:54:27 +0200

    feat(form): hover effects on buttons

--- app/components/Analysis/Form.vue
+++ app/components/Analysis/Form.vue
@@ -25,7 +25,7 @@ function clear() {
 <template>
   <form
     @submit.prevent="handleSubmit"
-    class="flex items-center gap-2 mb-8 border border-gh-border relative rounded-full px-6 py-3 focus-within:border-gh-border-light"
+    class="flex items-center gap-2 mb-8 border border-gh-border/60 relative rounded-full pl-6 pr-4 py-3 focus-within:border-gh-border-light"
   >
     <label class="flex-1" for="userName">
       <span class="sr-only">Enter account name</span>
@@ -42,13 +42,21 @@ function clear() {
         placeholder="Search accounts (e.g. torvalds)"
       />
     </label>
-    <div class="flex items-center gap-4">
-      <button v-if="accountName" type="button" @click="clear" class="flex">
+    <div class="flex items-center gap-2">
+      <button
+        v-if="accountName"
+        type="button"
+        @click="clear"
+        class="flex rounded-full p-1.5 hover:bg-gh-muted/20 transition-all"
+      >
         <span class="i-lucide:x" />
         <span class="sr-only">Clear input field</span>
       </button>
 
-      <button type="submit" class="flex">
+      <button
+        type="submit"
+        class="flex rounded-full p-1.5 hover:bg-gh-muted/20 transition-all"
+      >
         <span class="i-lucide-search" aria-hidden="true" />
         <span class="sr-only">Analyze</span>
       </button>
